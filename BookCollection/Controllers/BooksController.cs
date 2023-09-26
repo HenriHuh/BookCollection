2659764a23d1f898352cdcd3c5ffadf2e46df62f
HenriHuh <henkka.huhtala@hotmail.com>
2023-09-26 17:16:59 +0300

    - Base project

---
+++ BookCollection/Controllers/BooksController.cs
@@ -0,0 +1,96 @@
+﻿using BookCollection.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookCollection.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class BooksController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public BooksController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<Book>> GetBooks( string author = null, int? year = null, string publisher = null)
+        {
+            IQueryable<Book> query = _context.Books;
+
+            if (!string.IsNullOrEmpty(author))
+            {
+                query = query.Where(b => b.Author.Contains(author));
+            }
+
+            if (year.HasValue)
+            {
+                query = query.Where(b => b.Year == year.Value);
+            }
+
+            if (!string.IsNullOrEmpty(publisher))
+            {
+                query = query.Where(b => b.Publisher != null && b.Publisher.Contains(publisher));
+            }
+
+            var books = query.ToList();
+            return Ok(books);
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<Book> GetBook(int id)
+        {
+            var book = _context.Books.Find(id);
+
+            if (book == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(book);
+        }
+
+        [HttpPost]
+        public ActionResult<Book> PostBook(Book book)
+        {
+            if (string.IsNullOrWhiteSpace(book.Title) || string.IsNullOrWhiteSpace(book.Author))
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (_context.Books.Any(b => b.Title == book.Title && b.Author == book.Author))
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            _context.Books.Add(book);
+            _context.SaveChanges();
+
+            return Ok(new { id = book.Id });
+        }
+
+        [HttpDelete("{id}")]
+        public ActionResult DeleteBook(int id)
+        {
+
+            var book = _context.Books.Find(id);
+
+            if (book == null)
+            {
+                return NotFound();
+            }
+
+            _context.Books.Remove(book);
+            _context.SaveChanges();
+
+            return NoContent();
+        }
+    }
+
+}
---
+++ BookCollection/Models/Book.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+using System.Runtime.InteropServices;
+using System.Runtime.Serialization;
+
+namespace BookCollection.Models
+{
+
+    public class Book
+    {
+        public int Id { get; set; }
+
+        public required string Title { get; set; }
+
+        public required string Author { get; set; }
+
+        public required int Year { get; set; }
+
+        public string? Publisher { get; set; }
+
+        public string? Description { get; set; }
+
+
+    }
+
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Book> Books { get; set; }
+    }
+}
